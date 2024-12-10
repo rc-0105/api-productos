e676ffbc141f6bb091e1d94a03a6556d3c3c3c4b
ricardo <rcarrero@unbosque.edu.co>
2024-12-10 10:44:10 -0500

    first commit

---
+++ src/test/java/com/rc/dev/api_productos/ApiProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.dev.api_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
